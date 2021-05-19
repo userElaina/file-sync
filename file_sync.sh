2ce28db1f33791e238250fe41a92c35041940d62
Elaina <YaeSakura16771677@Gmail.com>
2021-05-19 19:48:14 +0800

    Add files via upload

---
+++ file_sync.sh
@@ -0,0 +1,7 @@
+cp -r "/root/log/from/bigfile" "/root/log/to/bigfile"
+cp "/root/log/to/exist/edit.txt" "/root/log/to/exist/edit.txt.00.sync_old"
+cp "/root/log/from/exist/edit.txt" "/root/log/to/exist/edit.txt"
+cp "/root/log/from/exist/nexist.txt" "/root/log/to/exist/nexist.txt"
+cp -r "/root/log/from/nexist" "/root/log/to/nexist"
+cp -r "/root/log/from/nexiste" "/root/log/to/nexiste"
+#/root/log/to/more.txt
